feaffa11c64704272d88f0b89112e3629be46eff
Анастасия Ельшина <salnaya92@gmail.com>
2023-10-11 02:35:23 +0300

    update useEffect

--- watches/vite-watches/src/components/Clock/Clock.tsx
+++ watches/vite-watches/src/components/Clock/Clock.tsx
@@ -1,61 +1,58 @@
 import { IClockProps } from "../../models/IClockProps";
-import { useState, useEffect } from 'react';
-import moment from 'moment';
+import { useState, useEffect } from "react";
+import moment from "moment";
 import ArrowPosition from "../ArrowPosition/ArrowPosition";
-import './Clock.css';
+import "./Clock.css";
 
 interface IClockPropsExtend {
-    item: IClockProps,
-    onDelete: CallableFunction;
+  item: IClockProps;
+  onDelete: CallableFunction;
 }
 
 export default function Clock({ item, onDelete }: IClockPropsExtend) {
-    const [time, setTime] = useState(new Date());
-    const { id, name, timezone } = item;
-    let timeout: number | undefined;
+  const [time, setTime] = useState(new Date());
+  const { id, name, timezone } = item;
+  let timeout: number | undefined;
 
-    const ticTac = () => {
-        setTime(new Date());
-    }
+  const ticTac = () => {
+    setTime(new Date());
+  };
 
-    const deleteClock = (evt: React.MouseEvent) => {
-        evt.preventDefault();
-        onDelete(id);
-      }
+  useEffect(ticTac, []);
 
-    useEffect(ticTac, []);
+  useEffect(() => {
+    timeout = setTimeout(ticTac, 1000);
 
-    useEffect(() => {
-        timeout = setTimeout(() => {
-            ticTac
-        }, 1000)
-        
-        return () => {
-            clearTimeout(timeout); 
-          }
-    })
+    return () => {
+      clearTimeout(timeout);
+    };
+  });
 
-  
-    
-    const timeOfZone = moment(time).utcOffset(timezone);
-    const hours = timeOfZone.hours();
-    const minutes = timeOfZone.minutes();
-    const seconds = timeOfZone.seconds();
+  const deleteClock = (evt: React.MouseEvent) => {
+    evt.preventDefault();
+    onDelete(id);
+  };
 
-    return (
-        <div className="clock">
-          <h4 className="clock-name">{name}</h4>
-          <div className="clock-face">
-            <ArrowPosition time={seconds} arrowType="seconds" />
-            <ArrowPosition time={minutes} arrowType="minutes" />
-            <ArrowPosition time={hours} arrowType="hours" />
-          </div>
-          <button
-            className="clock-btn"
-            type="button"
-            onClick={deleteClock}>X</button>
-        </div>
-      )
-    
+  const timeOfZone = moment(time).utcOffset(timezone);
+  const hours = timeOfZone.hours();
+  const minutes = timeOfZone.minutes();
+  const seconds = timeOfZone.seconds();
+
+  console.log(hours);
+  console.log(minutes);
+  console.log(seconds);
+
+  return (
+    <div className="clock">
+      <h4 className="clock-name">{name}</h4>
+      <div className="clock-face">
+        <ArrowPosition time={seconds} arrowType="seconds" />
+        <ArrowPosition time={minutes} arrowType="minutes" />
+        <ArrowPosition time={hours} arrowType="hours" />
+      </div>
+      <button className="clock-btn" type="button" onClick={deleteClock}>
+        X
+      </button>
+    </div>
+  );
 }
-
